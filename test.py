4f44f19372efe60a9d7aadb37e4ed245697b6cfc
seryoni <seryoni@gmail.com>
2018-06-18 08:19:52 +0300

    removing test prints

--- test.py
+++ test.py
@@ -3,17 +3,16 @@ import os
 import unittest
 from jumper.vlab import Vlab
 
-class TestReadInterrupt(unittest.TestCase):
+class TestDebouncer(unittest.TestCase):
     def setUp(self):
         # set up the device simulation
         self.v = Vlab(working_directory=".", print_uart=True, token="20Ew9+GBD5UwnjMZHoZgECQbEqAm5S4RlmM/GMeFvVk=")
         self.v.load("BUILD/STM32_Button_Debounce.bin")
         self.v.on_pin_level_event(self.pin_listener)
+        # running for a while to get the BSP done
+        self.v.run_for_ms(10)
         self.is_led_on = False
         self.times_pressed = 0
-        # running for a while to get the BSP done
-        self.v.run_for_ms(500)
-        print('Virtual device is ready')
 
     def tearDown(self):
         self.v.stop()
@@ -24,22 +23,16 @@ class TestReadInterrupt(unittest.TestCase):
             self.times_pressed = self.times_pressed + 1
 
     def test_debouncer(self):
-        print('Button on')
         self.v.UserButton.on()
         self.v.run_for_ms(10)
         self.v.UserButton.off()
-        print('Button off')
         self.v.run_for_ms(10)
-        print('Button on')
         self.v.UserButton.on()
         self.v.run_for_ms(10)
         self.v.UserButton.off()
-        print('Button off')
         self.v.run_for_ms(10)
-        print('Button on')
         self.v.UserButton.on()
         self.v.UserButton.off()
-        print('Button off')
         self.v.run_for_ms(280)
         self.assertEqual(self.is_led_on, True)
         self.assertEqual(self.times_pressed, 1)
