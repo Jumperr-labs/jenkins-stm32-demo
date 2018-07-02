1894811e6dab2432cee5394eaeacbb695ea82ffb
Dan Shemesh <dan@jumper.io>
2018-07-02 21:53:53 +0100

    test_hw

---
+++ test_hw.py
@@ -0,0 +1,27 @@
+import unittest
+import serial
+import subprocess
+import os
+from time import sleep
+
+
+class HwTest(unittest.TestCase):
+    def setUp(self):
+        self.serial = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1)
+        self.restart()
+
+    def restart(self):
+        openocd_dir = os.environ['OPENOCD_PATH']
+        openocd_bin = os.path.join(openocd_dir, 'bin', 'openocd')
+        args = [openocd_bin, '-f', 'scripts/board/st_nucleo_f4.cfg', '-c', 'init', '-c', 'reset', '-c', 'exit']
+        subprocess.check_call(args, cwd=openocd_dir)
+
+    def testSanity(self):
+        sleep(1)
+        expected_data = 'mBed boot done\n'
+        data = self.serial.read(len(expected_data))
+        self.assertEqual(data, expected_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
