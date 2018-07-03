11201db7d284a6ad8b667069907b42c17e355562
Dan Shemesh <dan@jumper.io>
2018-07-03 16:29:45 +0100

    fail

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ void button1_onpressed_cb(void)
 int main()
 {
 
-    printf("mBed boot done\n");
+//    printf("mBed boot done\n");
 
     //button1.mode(PullUp); // Activate pull-up
     button1.fall(callback(button1_onpressed_cb)); // Attach ISR to handle button press event
